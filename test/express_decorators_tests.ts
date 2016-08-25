e66d0b3defa5a2e6075fe4a4dfd0c9352ac2dd31
mserranom <miguel@plumbee.co.uk>
2016-08-25 11:42:47 +0200

    extracted test-services to new file

--- test/express_decorators_tests.ts
+++ test/express_decorators_tests.ts
@@ -1,22 +1,15 @@
 "use strict";
 
-import {Readable} from 'stream'
-
 import {Application} from 'express';
 
 import {Server} from 'http';
 
 import {configureObject, RequestMapping, Route, Middleware, GET, POST} from '../src/express_decorators';
 
+import {TestEndpoint, TestEndpoint2, TestEntity} from './test_services'
+
 import * as express from 'express';
 const bodyParser : any = require('body-parser');
-
-
-async function sleep(ms:number) : Promise<any> {
-    return new Promise<void>(function(resolve) {
-        setTimeout(function(){ resolve() }, ms);
-    });
-}
 
 function setupChai() {
     var chai = require('chai');
@@ -32,20 +25,15 @@ function setupChai() {
 var expect = require('chai').expect;
 var request : any = require('request-promise');
 
-
 setupChai();
-
 
 let app : express.Application;
 let server : Server;
 
 
-
-
 // ------------------
 // EXPRESS BOOTSTRAP
 // ------------------
-
 
 function startExpress(port : number) : Promise<void> {
 
@@ -74,123 +62,6 @@ function stop() : void {
     }
     server.close();
 }
-
-
-// ----------
-// TEST DATA
-// ----------
-
-let emptyMiddleware = (req, res, next) => next();
-
-function zeroIdCheckMiddleware(req, res, next) {
-    let body = req.body;
-    if(body['id'] === '0') {
-        res.status(413).send('id is zero');
-    }
-    next();
-}
-
-function nameNullCheckMiddleware(req, res, next) {
-    let body = req.body;
-    if(body['name'] === null) {
-        res.status(414).send('id is null');
-    }
-    next();
-}
-
-class TestEntity {
-    name : string = 'entity';
-    id : string = '101';
-}
-
-@Middleware(nameNullCheckMiddleware)
-class TestEndpoint {
-
-    private data : TestEntity = new TestEntity();
-
-    @RequestMapping('get', '/ping')
-    pong() : string {
-        return 'pong!';
-    }
-
-    @RequestMapping('POST', '/entities')
-    @Middleware([emptyMiddleware, zeroIdCheckMiddleware, emptyMiddleware])
-    setData(data : TestEntity) : void {
-        this.data = data;
-    }
-
-    @RequestMapping('GET', '/entities/:id')
-    getData(id : string) : TestEntity {
-        return (this.data.id === id) ? this.data : undefined;
-    }
-
-    @RequestMapping('GET', '/async_entities/:id')
-    async getAsyncData(id : string) : Promise<TestEntity> {
-        await sleep(1);
-        return (this.data.id === id) ? this.data : undefined;
-    }
-
-    @RequestMapping('GET', '/wrong')
-    throwError() : TestEntity {
-        throw new Error('error thrown');
-    }
-
-    @RequestMapping('GET', '/wrong_async')
-    async throwAsyncError() : Promise<void> {
-        await sleep(1);
-        throw new Error('error thrown');
-    }
-
-    @RequestMapping('GET', '/stream_data')
-    pipeData() : Readable {
-        let stream = new Readable();
-        stream._read = function noop() {}; // redundant? see update below
-        stream.push('data piped correctly!');
-        stream.push(null);
-        return stream;
-    }
-
-    @RequestMapping('get', '/numbers/:id',['from','to'])
-    sliceNumbers(id : string, from : number, to : number) : Array<number> {
-        let data = [3,4,5,6,7];
-        if(id == '101') {
-            return data.slice(from, to);
-        } else {
-            throw new Error('unknown id');
-        }
-    }
-
-    @GET('/header_data/:id',['query'], ['header1', 'header2'])
-    dataWithHeaders(id : string, query : string, header1 : string, header2 : string) : string {
-        if(id == 'myId' && query == 'myQuery' && header1 == 'myHeader1' && header2 == 'myHeader2') {
-            return 'pong';
-        } else {
-            throw new Error('unknown input');
-        }
-    }
-
-}
-
-@Route('/hi')
-@Middleware(emptyMiddleware)
-class TestEndpoint2 {
-
-    private message = 'hello!';
-
-    @RequestMapping('get')
-    @Middleware([emptyMiddleware])
-    getMessage() : string {
-        return this.message;
-    }
-
-    @POST()
-    setMessage(message : string) : void {
-        this.message = message;
-    }
-
-}
-
-
 
 // ----------
 // SPECS
---
+++ test/test_services.ts
@@ -0,0 +1,119 @@
+import {RequestMapping, Route, Middleware, GET, POST} from '../src/express_decorators';
+import {Readable} from 'stream'
+
+async function sleep(ms:number) : Promise<any> {
+    return new Promise<void>(function(resolve) {
+        setTimeout(function(){ resolve() }, ms);
+    });
+}
+
+let emptyMiddleware = (req, res, next) => next();
+
+function zeroIdCheckMiddleware(req, res, next) {
+    let body = req.body;
+    if (body['id'] === '0') {
+        res.status(413).send('id is zero');
+    }
+    next();
+}
+
+function nameNullCheckMiddleware(req, res, next) {
+    let body = req.body;
+    if (body['name'] === null) {
+        res.status(414).send('id is null');
+    }
+    next();
+}
+
+export class TestEntity {
+    name: string = 'entity';
+    id: string = '101';
+}
+
+@Middleware(nameNullCheckMiddleware)
+export class TestEndpoint {
+
+    private data: TestEntity = new TestEntity();
+
+    @RequestMapping('get', '/ping')
+    pong(): string {
+        return 'pong!';
+    }
+
+    @RequestMapping('POST', '/entities')
+    @Middleware([emptyMiddleware, zeroIdCheckMiddleware, emptyMiddleware])
+    setData(data: TestEntity): void {
+        this.data = data;
+    }
+
+    @RequestMapping('GET', '/entities/:id')
+    getData(id: string): TestEntity {
+        return (this.data.id === id) ? this.data : undefined;
+    }
+
+    @RequestMapping('GET', '/async_entities/:id')
+    async getAsyncData(id: string): Promise<TestEntity> {
+        await sleep(1);
+        return (this.data.id === id) ? this.data : undefined;
+    }
+
+    @RequestMapping('GET', '/wrong')
+    throwError(): TestEntity {
+        throw new Error('error thrown');
+    }
+
+    @RequestMapping('GET', '/wrong_async')
+    async throwAsyncError(): Promise<void> {
+        await sleep(1);
+        throw new Error('error thrown');
+    }
+
+    @RequestMapping('GET', '/stream_data')
+    pipeData(): Readable {
+        let stream = new Readable();
+        stream._read = function noop() {
+        }; // redundant? see update below
+        stream.push('data piped correctly!');
+        stream.push(null);
+        return stream;
+    }
+
+    @RequestMapping('get', '/numbers/:id', ['from', 'to'])
+    sliceNumbers(id: string, from: number, to: number): Array<number> {
+        let data = [3, 4, 5, 6, 7];
+        if (id == '101') {
+            return data.slice(from, to);
+        } else {
+            throw new Error('unknown id');
+        }
+    }
+
+    @GET('/header_data/:id', ['query'], ['header1', 'header2'])
+    dataWithHeaders(id: string, query: string, header1: string, header2: string): string {
+        if (id == 'myId' && query == 'myQuery' && header1 == 'myHeader1' && header2 == 'myHeader2') {
+            return 'pong';
+        } else {
+            throw new Error('unknown input');
+        }
+    }
+
+}
+
+@Route('/hi')
+@Middleware(emptyMiddleware)
+export class TestEndpoint2 {
+
+    private message = 'hello!';
+
+    @RequestMapping('get')
+    @Middleware([emptyMiddleware])
+    getMessage(): string {
+        return this.message;
+    }
+
+    @POST()
+    setMessage(message: string): void {
+        this.message = message;
+    }
+
+}
